798c2213b35ca12a6d4cb6c97c522dbf6c287cde
BulavkinaValeria <lerabulavkina.a@gmail.com>
2022-09-28 21:07:46 +0300

    testng1

--- src/test/java/AUnitTest.java
+++ src/test/java/AUnitTest.java
@@ -1,9 +1,15 @@
 import org.testng.annotations.Test;
 
-public class AUnitTest extends BaseTest{
+public class AUnitTest extends BaseTest {
 
     @Test
     public void firstTest() {
         System.out.println("Hello World!!!");
+    }
+
+    @Test
+    public void test() {
+        System.out.println("Test..");
+
     }
 }
