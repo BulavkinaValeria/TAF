c7cab582ce61ce381a8f98e33c83781683015766
BulavkinaValeria <lerabulavkina.a@gmail.com>
2022-11-22 21:31:22 +0300

    done

--- src/tests/java/baseEntities/BaseTest.java
+++ src/tests/java/baseEntities/BaseTest.java
@@ -2,11 +2,14 @@ package baseEntities;
 
 import configuration.ReadProperties;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.decorators.WebDriverDecorator;
+import org.testng.ITestContext;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Listeners;
 import services.BrowsersService;
 import steps.LoginStep;
+import steps.NavigatioStep;
 import utils.Listener;
 
 //@Listeners(Listener.class)
@@ -15,8 +18,9 @@ public class BaseTest {
     protected LoginStep loginStep;
 
     @BeforeMethod
-    public void setUp() {
+    public void setUp(ITestContext iTestContext) {
         driver = new BrowsersService().getDriver();
+        this.setDriverToContext(iTestContext, driver);
 
         driver.get(ReadProperties.getUrl());
 
@@ -27,5 +31,9 @@ public class BaseTest {
     @AfterMethod
     public void tearDown() {
         driver.quit();
+    }
+
+    public static void setDriverToContext(ITestContext iTestContext, WebDriver driver){
+        iTestContext.setAttribute("WebDriver", driver);
     }
 }
