80cd03b73f09170e3ab410894741adcaadb4fe75
BulavkinaValeria <lerabulavkina.a@gmail.com>
2022-11-27 13:37:00 +0300

    done

--- src/tests/java/baseEntities/BaseTest.java
+++ src/tests/java/baseEntities/BaseTest.java
@@ -1,39 +1,59 @@
 package baseEntities;
 
 import configuration.ReadProperties;
+import io.qameta.allure.Attachment;
+import io.qameta.allure.Description;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.decorators.WebDriverDecorator;
 import org.testng.ITestContext;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Listeners;
 import services.BrowsersService;
 import steps.LoginStep;
 import steps.NavigatioStep;
+import utils.InvokedListener;
 import utils.Listener;
 
-//@Listeners(Listener.class)
+@Listeners(InvokedListener.class)
 public class BaseTest {
     protected WebDriver driver;
     protected LoginStep loginStep;
 
-    @BeforeMethod
+    @BeforeMethod(description = "Настройка")
+    @Description("Настройка")
     public void setUp(ITestContext iTestContext) {
         driver = new BrowsersService().getDriver();
-        this.setDriverToContext(iTestContext, driver);
+        // Solution 1
+        iTestContext.setAttribute("driver", driver);
+        // Solution 1 - Finish
 
         driver.get(ReadProperties.getUrl());
 
         loginStep = new LoginStep(driver);
-
     }
 
-    @AfterMethod
-    public void tearDown() {
+    @AfterMethod(description = "Завершение")
+    public void tearDown(ITestResult testResult) {
+        // Solution - 2: Плохое решение - потому, что Screenshot добавляется в шаг TearDown
+        /*
+        if (testResult.getStatus() == ITestResult.FAILURE) {
+            try {
+                byte[] srcFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
+                saveScreenshot(srcFile);
+            } catch (NoSuchSessionException ex) {
+
+            }
+        }
+        */
         driver.quit();
     }
 
-    public static void setDriverToContext(ITestContext iTestContext, WebDriver driver){
-        iTestContext.setAttribute("WebDriver", driver);
+    // Solution - 2:
+    @Attachment(value = "Page screenshot", type = "image/png")
+    private byte[] saveScreenshot(byte[] screenshot) {
+        return screenshot;
     }
+    // Solution - 2: Finish
 }
